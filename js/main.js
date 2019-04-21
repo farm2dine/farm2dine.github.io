6f075546fdbce058703c70673f13fa263a3d344a
Gunasekaran R <gunasekaran.r@thetechchefs.in>
2019-04-21 17:47:24 +0530

    dev commit -1

---
+++ js/main.js
@@ -0,0 +1,66 @@
+$('document').ready(function(e){
+	document.onscroll = function(e) {
+		updateNavColor();
+	}
+	updateNavColor();
+});
+function updateNavColor() {
+	if(window.scrollY > 200) {
+		$('nav.navbar').addClass('scroll');
+	} else if($('nav.navbar').hasClass('scroll')) {
+		$('nav.navbar').removeClass('scroll');
+	}
+}
+
+
+//---------- update content from google sheet
+var globalData = {};
+var sheetKey = "1Y5gtD0hx9_HVPn8z2xhE94isolQvizszYNrcE8b8MC0";
+var apiKey = "AIzaSyDarLhb6rGyePmRz2oHAltaZSQYjElyATQ";
+var sheetUrl = "https://sheets.googleapis.com/v4/spreadsheets/"+sheetKey+"/values/";
+
+var discountElm = "discount-list";
+
+//discount data
+getsheetData(sheetUrl+"discounts").then(function(data){
+	var tempStr = updateTemplate($("#"+discountElm).html(), data),
+		tempElm = document.getElementById(discountElm);
+
+	tempElm.innerHTML = tempStr.join(" ");
+});
+
+
+
+
+
+function getsheetData(url) {
+	var getProm = new Promise(function(resolve, reject) {
+		return $.get(url+"?key="+apiKey,function(data) {
+			var arr = [], val = data.values, feilds = data.values[0];
+			for(var i=1; i < val.length; i++) {
+				var row = {};
+
+				feilds.map(function(v,inner){
+					row[v] = val[i][inner] || "N/A";
+				});
+
+				arr.push(row);
+			}
+			resolve(arr);
+		});
+	});
+	return getProm;
+};
+
+function updateTemplate(template, data) {
+	var tempArray = [];
+	data.map(function(v,i){
+
+		var allKeys = Object.keys(v), t = template;
+		allKeys.map(function(k,inn){
+			t = t.replace("{{"+k+"}}", v[k]);
+		});
+		tempArray.push(t);
+	});
+	return tempArray;
+}
